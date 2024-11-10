723080d9289bb069ed73369a076609f74e55ff8a
Marioo12 <alexandermariosaputra@gmail.com>
2024-11-10 16:28:50 +0700

    first commit

---
+++ Object Repository/Page_Berita makanan Terbaru dan Terkini Har_55c5ee/h3_Ekonomi Melambat, Industri Makanan dan M_a9ab74.rs
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h3_Ekonomi Melambat, Industri Makanan dan M_a9ab74</name>
+   <tag></tag>
+   <elementGuidId>b7241b05-5b06-42ef-bc41-d22e6f72028a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//body[@id='page-top']/div[6]/div/section/div/div/div/article/div[2]/a/h3</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>h3.content-title.content-title--list.mb-3</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;Ekonomi Melambat, Industri Makanan dan Minuman Diprediksi Masih Bisa Tumbuh 5%&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h3</value>
+      <webElementGuid>217b2654-e65f-401a-adab-929953459d6c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>content-title content-title--list mb-3</value>
+      <webElementGuid>92d20d92-0cf8-4182-b408-1141ec3f1a70</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Ekonomi Melambat, Industri Makanan dan Minuman Diprediksi Masih Bisa Tumbuh 5%</value>
+      <webElementGuid>7a7af744-3f4c-48f1-85ff-24612e3bc79e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;page-top&quot;)/div[@class=&quot;content-main&quot;]/div[@class=&quot;contentWrapper&quot;]/section[@class=&quot;search-results&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-md-9 latest-news&quot;]/article[@class=&quot;article article--berita d-flex&quot;]/div[@class=&quot;content-text&quot;]/a[1]/h3[@class=&quot;content-title content-title--list mb-3&quot;]</value>
+      <webElementGuid>a37eab02-969d-4790-8234-e8cc7dd7efbb</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//body[@id='page-top']/div[6]/div/section/div/div/div/article/div[2]/a/h3</value>
+      <webElementGuid>b64002f4-f09a-4224-9539-ac400c1ad856</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Disclaimer'])[1]/following::h3[1]</value>
+      <webElementGuid>5b8c48f8-b750-4256-bf02-3d03121c5c14</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Kebijakan Privasi'])[1]/following::h3[1]</value>
+      <webElementGuid>9c50c027-f546-40a9-beb4-8539c88778de</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Industri'])[3]/preceding::h3[1]</value>
+      <webElementGuid>b89cb118-30e5-4c3e-96a1-2a40892212f0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='• 5 November 2024, 16.31'])[1]/preceding::h3[1]</value>
+      <webElementGuid>85344bd6-ea07-4cb4-a10c-496a0d7de198</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Ekonomi Melambat, Industri Makanan dan Minuman Diprediksi Masih Bisa Tumbuh 5%']/parent::*</value>
+      <webElementGuid>685d4989-32fd-4e02-915c-2eefb7c5d16c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//h3</value>
+      <webElementGuid>ed7a95ba-f756-4f70-9d62-0e5ec237ac84</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h3[(text() = 'Ekonomi Melambat, Industri Makanan dan Minuman Diprediksi Masih Bisa Tumbuh 5%' or . = 'Ekonomi Melambat, Industri Makanan dan Minuman Diprediksi Masih Bisa Tumbuh 5%')]</value>
+      <webElementGuid>d3222421-e4dc-419f-abe1-09480ff3c496</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Page_Berita makanan Terbaru dan Terkini Har_55c5ee/path.rs
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>path</name>
+   <tag></tag>
+   <elementGuidId>55aa1326-1e4e-482d-b1ec-f34efd09665c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>path</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:label=&quot;Tutup iklan&quot;i</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>path</value>
+      <webElementGuid>8345464c-966f-400c-92e7-48fe5bbf6074</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>d</name>
+      <type>Main</type>
+      <value>M38 12.83L35.17 10 24 21.17 12.83 10 10 12.83 21.17 24 10 35.17 12.83 38 24 26.83 35.17 38 38 35.17 26.83 24z</value>
+      <webElementGuid>adcd50ac-2775-454c-a79b-e3fd1b12f23b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;dismiss-button&quot;)/div[1]/svg[1]/path[1]</value>
+      <webElementGuid>6c665dbd-ce88-4d86-a0c7-341e1b1281b2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ref_element</name>
+      <type>Main</type>
+      <value>Object Repository/Page_Berita makanan Terbaru dan Terkini Har_55c5ee/iframe_2024 Katadata. Hak cipta dilindungi _f3ea9e</value>
+      <webElementGuid>6487d0fc-642a-47d6-9904-0cd10eea240f</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
